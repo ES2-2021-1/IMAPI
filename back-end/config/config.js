1a1a133ade71e7a8b4d7d798a5626418eb7fa843
Alisson <alisson.luan2000@gmail.com>
2021-08-16 13:55:51 -0300

    adjust config path

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -1,8 +1,8 @@
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "teste",
+    "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
