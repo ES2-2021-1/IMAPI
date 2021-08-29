6ecbe87633334e6cc1d7ba3e0e7955a9b953da83
Guilherme Graeff <guilherme.rafael.graeff@gmail.com>
2021-08-29 18:35:32 -0300

    Add Model e Migration

---
+++ back-end/src/migrations/20210826170113-create-submission.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Submissions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      submissionDate: {
+        type: Sequelize.DATE
+      },
+      feedBack: {
+        type: Sequelize.TEXT
+      },
+      taskId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        referencer: {
+          model: {
+            tableName: 'Tasks'
+          },
+          key: 'id'
+        }
+      },
+      startupId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        referencer: {
+          model: {
+            tableName: 'Sturtups'
+          },
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Submissions');
+  }
+};
