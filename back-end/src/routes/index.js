e8e8ce648720c23e01bba903449910e3d9025c61
Stefani Meneghetti <meneghettistefani@gmail.com>
2021-09-08 21:04:59 -0300

    feat: adiciona controller de anexos

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const AnnexRoutes = require('./AnnexRoutes');
 
-
+router.use('/annex', AnnexRoutes);
 
 module.exports = router;
