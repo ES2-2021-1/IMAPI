b020ff7a4999f22320a738f6d06948dce2868d24
Guilherme Graeff <guilherme.rafael.graeff@gmail.com>
2021-09-09 15:53:07 -0300

    create routes sem testar, pq eu n to conseguindo, só copiei e colei mesmo poucas ideias, n sou bom nisso oke

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const UserRoutes = require('./UserRoutes');
 const StartupRoutes = require('./StartupRoutes');
+const SubmissionRoutes = require('./SubmissionRoutes');
 
 router.use('/user', UserRoutes);
 router.use('/startup', StartupRoutes);
+router.use('/submission', SubmissionRoutes);
 
 module.exports = router;
