4a4bed56438092c7e00ff4938a9ada1fa113c097
Alisson <alisson.luan2000@gmail.com>
2021-09-09 15:03:39 -0300

    New route: Auth

---
+++ back-end/src/controllers/AuthController.js
@@ -0,0 +1,52 @@
+var jwt = require('jsonwebtoken');
+const User = require("../models").User;
+const Validator = require('validatorjs');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+class AuthController{
+    async store (req, res) {
+        let data = req.body;
+        const rules = {
+            'email': 'required|string',
+            'password': 'required|string',
+        };
+
+        let validator = new Validator(req.body, rules);
+
+        if (validator.fails()){
+            res.status(400);
+            res.send(validator.errors);
+            return;
+        }
+
+        let user = await User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        });
+
+        if(user == null){
+            res.status(401);
+            res.json({"error": "Invalid email or password"});
+            return;
+        }
+
+        bcrypt.compare(req.body.password, user.password, function(err, result) {
+            if (result){
+                let token = jwt.sign({"id": user.id}, process.env.JWT_SECRET, {expiresIn: process.env.SESSION_LIFETIME});
+                res.status(200);
+                res.json({"token":token});
+                return;
+            }
+            if(err){
+                console.log(err);
+            }
+            res.status(401);
+            res.json({"error": "Invalid email or password"});
+            return;
+        });
+    }
+}
+
+module.exports = new AuthController();
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -5,10 +5,12 @@ const UserRoutes = require('./UserRoutes');
 const StartupRoutes = require('./StartupRoutes');
 const TaskRoutes = require('./TaskRoutes');
 const AnnexRoutes = require('./AnnexRoutes');
+const AuthRoutes = require('./AuthRoutes');
 
 router.use('/user', UserRoutes);
 router.use('/startup', StartupRoutes);
 router.use('/annex', AnnexRoutes);
 router.use('/task', TaskRoutes);
+router.use('/auth', AuthRoutes);
 
 module.exports = router;
