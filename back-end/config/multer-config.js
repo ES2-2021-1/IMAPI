36482ae37d845e01060cdf8849afdd918da1f6c3
Alisson <alisson.luan2000@gmail.com>
2021-09-16 13:46:26 -0300

    modify public/uploads to front-end

--- back-end/config/multer-config.js
+++ back-end/config/multer-config.js
@@ -6,7 +6,7 @@ relative_destination_path = 'public/uploads'
 multerConfig = {
     storage: multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null,  path.resolve(__dirname, '../', relative_destination_path))
+            cb(null,  path.resolve(__dirname, '../../front-end/', relative_destination_path));
         },
         filename: (req, file, cb) => {
             const uniqueName = Date.now() +'-'+ Math.round(Math.random() * 1E9) +'-'+ Math.round(Math.random() * 1E9);
