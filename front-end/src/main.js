28fe6baec0f60ea9a1f412f0079f372dd8cfb038
Alisson <alisson.luan2000@gmail.com>
2021-09-16 16:21:24 -0300

    add bootstrap vue

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(BootstrapVue);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
