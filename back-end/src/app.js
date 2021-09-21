d4a0286bc743544fcf113ee6fae38e439b28c0f4
Rafael Fernandes <rflgf0@gmail.com>
2021-09-20 21:42:57 -0300

    Adiciona a chamada do Cors.

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const routes = require("./routes");
-
+const  cors = require('cors');
 const app = express();
 
 dotenv.config();
+
+app.use(cors());
 app.use("/api", routes);
 
 
