426b53c321aa74b62fdeb9fc87699198ba9bb90a
Stefani Meneghetti <meneghettistefani@gmail.com>
2021-09-22 16:58:55 -0300

    feat: adiciona tratamento basico para as rotas da aplicaçao

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,11 +4,24 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import VueRouter from 'vue-router';
+
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import  router from './router';
 
 Vue.use(BootstrapVue);
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
+  router,
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return router[this.currentRoute]
+    }
+  },
 }).$mount('#app')
