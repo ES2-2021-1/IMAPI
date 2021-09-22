275c5c506da6e5f1797f67fc5a65d7d20fdac21e
Alisson <alisson.luan2000@gmail.com>
2021-09-22 20:32:41 -0300

    remove duplicate cors

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 const routes = require("./routes");
 const  cors = require('cors');
 const app = express();
-const cors = require('cors')
 
 app.use(cors());
 app.use(express.json());
