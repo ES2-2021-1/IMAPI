2574c74bdfe00a68ea1c9e0236214020658db1b9
Guilherme Graeff <guilherme.rafael.graeff@gmail.com>
2021-09-23 00:48:13 -0300

    mais um commit e zas e zas

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "database": "database",
-    "host": "localhost",
-    "dialect": "sqlite"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
--- front-end/src/pages/register/Register.vue
+++ front-end/src/pages/register/Register.vue
@@ -1,17 +1,29 @@
-
 <template>
 	<div class="form-login">
 		<section class="login-dark">
 		<form @submit.prevent="login">
 			<h2 class="visually-hidden">Login Form</h2>
 			<div class="illustration"><img :src="require('@/assets/img/Logo_TIPO BRANCO.png')" style="width: 100%;" required></div>
-			<div class="mb-3"><input v-model="name" class="form-control" type="text" name="name" placeholder="Nome" autocomplete="off" required></div>
-			<div class="mb-3"><input v-model="email" class="form-control" type="text" name="email" placeholder="Email" autocomplete="off" required></div>
-			<div class="mb-3"><input v-model="cpf"   class="form-control" type="text" name="cpf" placeholder="Cpf" autocomplete="off" required></div>
-			<div class="mb-3"><input v-model="phone"  class="form-control" type="text" name="phone" placeholder="Telefone" autocomplete="off" required></div>
-			<div class="mb-3"><input v-model="password" maxlength="32" class="form-control" type="password" name="password" placeholder="Senha" autocomplete="off" required></div>
-			<div class="mb-3"><input v-model="password_confirmation" maxlength="32" class="form-control" type="password" name="password_confirmation" placeholder="Confirmar Senha" autocomplete="off" required></div>
-			<div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit" style="background: var(--bs-success);">Cadastro</button></div>
+
+			<div class="mt-3"><input v-model="name" class="form-control" type="text" name="name" placeholder="Nome" autocomplete="off" required></div>
+			<small class="text-danger">{{ nameError }}</small>
+			
+			<div class="mt-3"><input v-model="email"  class="form-control" type="text" name="email" placeholder="Email" autocomplete="off" required></div>
+			<small class="text-danger">{{ emailError }}</small>
+			
+			<div class="mt-3"><input v-model="cpf" v-mask="'###.###.###-##'"  class="form-control" type="text" name="cpf" placeholder="Cpf" autocomplete="off" required></div>
+			<small class="text-danger">{{ cpfError }}</small>
+			
+			<div class="mt-3"><input v-model="phone" v-mask="'(##)#####-####'" class="form-control" type="text" name="phone" placeholder="Telefone" autocomplete="off" required></div>
+			<small class="text-danger">{{ phoneError }}</small>
+			
+			<div class="mt-3"><input v-model="password" maxlength="30" class="form-control" type="password" name="password" placeholder="Senha" autocomplete="off" required></div>
+			<small class="text-danger">{{ passwordError }}</small>
+			
+			<div class="mt-3"><input v-model="password_confirmation" maxlength="30" class="form-control" type="password" name="password_confirmation" placeholder="Confirmar Senha" autocomplete="off" required></div>
+			<small class="text-danger">{{ password_confirmationError }}</small>
+
+			<div class="mt-3"><button class="btn btn-primary d-block w-100" type="submit" style="background: var(--bs-success);">Cadastrar</button></div>
 	</form>
 	</section>
 </div>
@@ -24,73 +36,70 @@ export default {
 		return {
 				name: '',
 				email: '',
+				cpf: '',
+				phone: '',
 				password: '',
 				password_confirmation: '',
-				phone: '',
-				cpf: '',
-				role: '0'
+				role: '0',
+
+				nameError: ' ',
+				emailError: ' ',
+				cpfError: ' ',
+				phoneError: ' ',
+				passwordError: ' ',
+				password_confirmationError: ' ',
 			}
-		},
-		methods: {
-			async login() {
-				console.log(this.name);
-				console.log(this.email);
-				console.log(this.password);
-				console.log(this.cpf);
-				console.log(this.phone);
-				console.log(this.password);
-				console.log(this.password_confirmation);
+	},
+	methods: {
+		async login() {
+			this.nameError = '';
+			this.emailError = '';
+			this.cpfError = '';
+			this.phoneError = '';
+			this.passwordError = '';
+			this.password_confirmationError = '';
 
+			let newCpf = this.cpf.replaceAll(".", "");
+			newCpf = newCpf.replaceAll("-", "");
+			console.log(newCpf);
 
-				// const bodyParameters = {
-				// 	name: this.name,
-				// 	email: this.email,
-				// 	password: this.password,
-				// 	password_confirmation: this.password_confirmation,
-				// 	phone: this.phone,
-				// 	cpf: this.cpf,
-				// 	role: this.role,
-				// };
+			const bodyParameters = {
+				name: this.name,
+				email: this.email,
+				cpf: newCpf,
+				phone: this.phone,
+				password: this.password,
+				password_confirmation: this.password_confirmation,
+				role: this.role,
+			};
 
-				// window.axios.post( 
-				// 	'http://localhost:8082/api/user',
-				// 	bodyParameters,
-				// ).then( res => {
-				// 	console.log('then')
-				// 	console.log(res)
-				// }).catch(res => {
-				// 	console.log('catch')
-				// 	console.log(res)
-				// });
-				const name = this.name;
-				const email = this.email;
-				const password = this.password;
-				const password_confirmation = this.password_confirmation;
-				const phone = this.phone;
-				const cpf = this.cpf;
-				const role = this.role;
-				
-				const res = await fetch(
-					"http://localhost:8082/api/user",
-					{
-						method: "POST",
-						headers: {
-							"Content-Type": "application/json"
-						},
-						body: JSON.stringify({
-							name,
-							email,
-							password,
-							password_confirmation,
-							phone,
-							cpf,
-							role
-						})
+			window.axios.post( 
+				'http://localhost:8082/api/user',
+				bodyParameters
+				).then( res=> {
+					console.log(res)
+				}).catch( err => {
+					console.log("Erro: "+ JSON.stringify(err.response.data))
+					if(err.response.data.errors.name){
+						this.nameError = err.response.data.errors.name[0];
 					}
-				);
-				const data = await res.json();
-				console.log(data);
-			}
+					if(err.response.data.errors.email){
+						this.emailError = err.response.data.errors.email[0];
+					}
+					if(err.response.data.errors.cpf){
+						this.cpfError = err.response.data.errors.cpf[0];
+					}
+					if(err.response.data.errors.phone){
+						this.phoneError = err.response.data.errors.phone[0];
+					}
+					if(err.response.data.errors.password){
+						this.passwordError = err.response.data.errors.password[0];
+					}
+					if(err.response.data.errors.password_confirmation){
+						this.password_confirmationError = err.response.data.errors.password_confirmation[0];
+					}
+				});
+		}
 	}
 }
 </script>
